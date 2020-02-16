2d7e7b3bd1ca1c269794c237a4769e867ae13320
someOne404 <jiayilu96@gmail.com>
2020-02-15 20:30:16 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    //initialize the state object
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    //required to implement render()
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
